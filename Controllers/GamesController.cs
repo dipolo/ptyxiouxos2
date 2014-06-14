9038a65ac0f626b52e7d421c7c8a9d1051360f19
dipolo <dipologames@gmail.com>
2014-06-15 01:40:55 +0300

    Πτυχιούχος 2 - δοκιμή 1η
    Ανέβασμα όλου του project από το Visual Studio. Οδηγίες στο setup.txt

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ptyxiouxos2.Controllers
+{
+    public class GamesController : Controller
+    {
+        //
+        // GET: /Games/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tangram()
+        {
+            ViewBag.Message = "Tangram page.";
+            return View();
+        }
+
+	}
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ptyxiouxos2.Startup))]
+namespace Ptyxiouxos2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
